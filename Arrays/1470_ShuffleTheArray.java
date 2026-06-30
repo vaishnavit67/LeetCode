12df7c3152b9c7a43035d19f52ce514ea006a26c
Vaishnavi Tallada <vaishnavi.tallada@gmail.com>
2026-07-01 00:20:41 +0530

    Enough.. last question on arrays for today

--- Arrays/1470_ShuffleTheArray.java
+++ Arrays/1470_ShuffleTheArray.java
@@ -1,6 +1,6 @@
 // The Question
 /*
-1470. Shuffle the Array
+1470. Shuffle the Array (Easy)
 
 Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
 Return the array in the form [x1,y1,x2,y2,...,xn,yn].
---
+++ Arrays/1512_NumberOfGoodPairs.java
@@ -0,0 +1,46 @@
+// The Question
+/*
+1512. Number of Good Pairs (Easy)
+
+Given an array of integers nums, return the number of good pairs.
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+Example 1:
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+Example 2:
+Input: nums = [1,1,1,1]
+Output: 6
+Explanation: Each pair in the array are good.
+
+Example 3:
+Input: nums = [1,2,3]
+Output: 0
+
+Constraints:
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+*/
+
+// The Approach
+/*
+As the given constraint interval is very low, you could directly apply brute force
+Create an array that could store 100 values
+For every element in the given array, you add the no:of times the value has occured previously
+Finally return the no:of good pairs found
+*/
+
+// The Solution
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int n = nums.length;
+        int count[] = new int[101];
+        int goodPairs = 0;
+        for(int num : nums){
+            goodPairs += count[num]++;
+        }
+        return goodPairs;
+    }
+}
