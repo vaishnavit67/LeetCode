7d1de3800b50c7f374286386af189b3dbdfd6fe1
Vaishnavi Tallada <vaishnavi.tallada@gmail.com>
2026-06-30 23:55:58 +0530

    2D array sum calculation

--- Arrays/1480_RuninngSumOf1DArray.java
+++ Arrays/1480_RuninngSumOf1DArray.java
@@ -1,6 +1,6 @@
 // The Question
 /*
-1480. Running Sum of 1d Array
+1480. Running Sum of 1d Array (Easy)
 
 Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
 Return the running sum of nums.
