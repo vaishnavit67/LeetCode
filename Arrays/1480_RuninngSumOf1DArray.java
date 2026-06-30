f71b66025aa920e9f2b34487a417f389ec47639e
Vaishnavi Tallada <vaishnavi.tallada@gmail.com>
2026-06-30 23:48:36 +0530

    Just computing the prefix sum

---
+++ Arrays/1480_RuninngSumOf1DArray.java
@@ -0,0 +1,41 @@
+// The Question
+/*
+1480. Running Sum of 1d Array
+
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+Return the running sum of nums.
+
+Example 1:
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+Example 2:
+Input: nums = [1,1,1,1,1]
+Output: [1,2,3,4,5]
+Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+
+Example 3:
+Input: nums = [3,1,2,10,1]
+Output: [3,4,6,16,17]
+
+Constraints:
+1 <= nums.length <= 1000
+-10^6 <= nums[i] <= 10^6
+*/
+
+// The Approach
+/*
+Start from ind 1 as ind 0 would be the same in the prefix sum
+    for every ind i add the current element (nums[i]) to the previous sum computed till now (nums[i-1])
+*/
+
+// The Solution
+class Solution {
+    public int[] runningSum(int[] nums) {
+        for(int i = 1; i < nums.length; i++){
+            nums[i] = nums[i] + nums[i-1];
+        }
+        return nums;
+    }
+}
